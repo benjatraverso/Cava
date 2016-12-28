f913ddc015e5d0f98985f44c16c576e586408fb6
Ezequiel Blanco <kiel814@gmail.com>
2016-12-28 12:41:55 -0300

    logica y cosas random

---
+++ Arduino/Cava/Cava.ino
@@ -0,0 +1,20 @@
+const int S_CURRE = 7;
+
+void setup()
+{
+}
+
+void loop()
+{
+  switch (state)
+  {
+    case CURRENT:
+    break;
+    case UP:
+    break;
+    case DOWN:
+    break;
+    case DESIRED:
+    break;
+  }
+}
